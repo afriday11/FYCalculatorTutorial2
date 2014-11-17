722ea76cd98af30c60dab11fac972a8539d06fbf
andrew <afriday11@gmail.com>
2014-11-17 10:52:19 +0800

    last minute changes before tutoring session.

--- FVCalculatorTutorial2/FYNumber.h
+++ FVCalculatorTutorial2/FYNumber.h
@@ -2,16 +2,12 @@
 //  FYNumber.h
 //  FVCalculatorTutorial2
 //
-//  Created by Andrew Friday on 11/13/14.
+//  Created by Andrew Friday on 11/17/14.
 //  Copyright (c) 2014 Friday. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface FYNumber : NSObject
-
-@property (strong, nonatomic) NSString *displayNumber;
-@property (strong, nonatomic) NSString *hiddenNumber;
-
 
 @end
--- FVCalculatorTutorial2/FYNumber.m
+++ FVCalculatorTutorial2/FYNumber.m
@@ -2,14 +2,12 @@
 //  FYNumber.m
 //  FVCalculatorTutorial2
 //
-//  Created by Andrew Friday on 11/13/14.
+//  Created by Andrew Friday on 11/17/14.
 //  Copyright (c) 2014 Friday. All rights reserved.
 //
 
 #import "FYNumber.h"
 
 @implementation FYNumber
-
-
 
 @end
